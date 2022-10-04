3416cb185cd5bb5fdf69a55b5b79bdb03fede81d
StevenMcgrew <stevenmcgrew8224@msn.com>
2022-10-03 20:58:09 -0700

    Sidebar open/close handlers

--- scripts.js
+++
@@ -1,2 +0,0 @@
-// Halfmoon sidebar-overlay click handler
-document.querySelector('.sidebar-overlay').addEventListener('click', () => { halfmoon.toggleSidebar() })
---
+++ scripts/scripts.js
@@ -0,0 +1,3 @@
+// Halfmoon sidebar handlers
+document.querySelector('.sidebar-overlay').addEventListener('click', () => { halfmoon.toggleSidebar() })
+document.getElementById('sidebarBtn').addEventListener('click', () => { halfmoon.toggleSidebar() })
