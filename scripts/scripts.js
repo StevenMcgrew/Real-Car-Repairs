b8b7c0f6b6183a9edc5e1f031e6ca49d31bab6b6
StevenMcgrew <stevenmcgrew8224@msn.com>
2022-10-04 20:51:37 -0700

    Base layout and styles

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,4 @@
-// Halfmoon sidebar handlers
+// Halfmoon ui handlers
 document.querySelector('.sidebar-overlay').addEventListener('click', () => { halfmoon.toggleSidebar() })
 document.getElementById('sidebarBtn').addEventListener('click', () => { halfmoon.toggleSidebar() })
+document.getElementById('darkModeBtn').addEventListener('click', () => { halfmoon.toggleDarkMode() })
