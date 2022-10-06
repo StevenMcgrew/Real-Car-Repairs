6dff662fc47cbc3a451d0c4bc6e6415ed1615d61
StevenMcgrew <stevenmcgrew8224@msn.com>
2022-10-06 13:09:02 -0700

    Mini sidebar layout and styles

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,4 @@
 // Halfmoon ui handlers
 document.querySelector('.sidebar-overlay').addEventListener('click', () => { halfmoon.toggleSidebar() })
-document.getElementById('sidebarBtn').addEventListener('click', () => { halfmoon.toggleSidebar() })
-document.getElementById('darkModeBtn').addEventListener('click', () => { halfmoon.toggleDarkMode() })
+document.querySelector('.toggle-sidebar-btn').addEventListener('click', () => { halfmoon.toggleSidebar() })
+document.querySelector('.darkmode-btn').addEventListener('click', () => { halfmoon.toggleDarkMode() })
