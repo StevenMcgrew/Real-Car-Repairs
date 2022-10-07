69f52dd3c74ce2645e8444665d8cdc51034625ba
StevenMcgrew <stevenmcgrew8224@msn.com>
2022-10-07 10:37:51 -0700

    Responsive UI adjustments

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,6 @@
 // Halfmoon ui handlers
 document.querySelector('.sidebar-overlay').addEventListener('click', () => { halfmoon.toggleSidebar() })
-document.querySelector('.toggle-sidebar-btn').addEventListener('click', () => { halfmoon.toggleSidebar() })
 document.querySelector('.darkmode-btn').addEventListener('click', () => { halfmoon.toggleDarkMode() })
+document.querySelectorAll('.toggle-sidebar-btn').forEach((el) => {
+    el.addEventListener('click', () => { halfmoon.toggleSidebar() })
+})
